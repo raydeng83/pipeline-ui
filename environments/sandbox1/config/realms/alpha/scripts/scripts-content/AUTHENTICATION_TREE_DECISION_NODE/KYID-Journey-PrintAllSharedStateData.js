9cc226be2d5c39a4401b1606802ab8b92cefdb05
raydeng83 <ray.deng83@gmail.com>
2026-04-10 22:31:52 -0400

    pull(sandbox1): access-config, endpoints, email-templates, variables, idm-authentication, iga-workflows, journeys, managed-objects, scripts @ 2026-04-11T02:30:18Z — 298 items across 2 scopes (Scripts, Variables (ESVs))\n\n  Scripts: ~1\n  Variables (ESVs): ~297\n\nFull details saved to history.

--- environments/sandbox1/config/realms/alpha/scripts/scripts-content/AUTHENTICATION_TREE_DECISION_NODE/KYID-Journey-PrintAllSharedStateData.js
+++ environments/sandbox1/config/realms/alpha/scripts/scripts-content/AUTHENTICATION_TREE_DECISION_NODE/KYID-Journey-PrintAllSharedStateData.js
@@ -2,8 +2,8 @@
   - Data made available by nodes that have already executed are available in the sharedState variable.
   - The script should set outcome to either "true" or "false".
  */
-logger.error("KYID-Journey-PrintAllSharedStateData ::::::::::");
+logger.debug("KYID-Journey-PrintAllSharedStateData ::::::::::");
 var email = nodeState.get("mail")
 nodeState.putShared("username", email )
-logger.error("KYID-Journey-PrintAllSharedStateData ::::::::::"+nodeState.get("username"))
+logger.debug("KYID-Journey-PrintAllSharedStateData ::::::::::"+nodeState.get("username"))
 outcome = "true";
